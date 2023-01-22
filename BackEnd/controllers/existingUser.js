5c187d87cfc25f9ee9e194c31ec3325228537641
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-01-22 17:53:27 +0530

    task 5

--- BackEnd/controllers/existingUser.js
+++ BackEnd/controllers/existingUser.js
@@ -12,12 +12,24 @@ exports.login = async (req, res, next) => {
         if (stringInvalid(password) || stringInvalid(mail)) {
             return res.status(400).json({ err: "Missing input parameters" })
         }
+        const findUser = await user.findOne({ where: { email: mail } })
+            .then(user => {
+                if (user) {
+                    if (findUser.password == password) {
+                        return res.status(201).send('Login Succesfull');
+                    }
+                    else {
+                        return res.status(400).send('Login Unsuccesfull since wrong Password');
+                    }
 
-        res.status(201).json({ newUserDetail: data });
+                } else {
+                    return res.status(500).send('User Not found');
+                }
+            });
     } catch (err) {
         console.log(err);
         res.status(500).json({
-            error: 'Oopss! User exists Already!! Login'
+            error: 'Oopss!Searching not succeed!'
         })
     }
 }
